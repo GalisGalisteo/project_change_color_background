1bb61c85d852454f7929ad74b13b2ac07bccbc34
GalisGalisteo <galis.travelling@gmail.com>
2023-10-18 12:34:53 +0200

    ex finished

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 //En este array de strings, tenemos todos los colores
 const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 
+// TO YELLOW
+// const changeBgColor = () => {
+//     document.querySelector('body').style.background = 'yellow';
+// }
 
+
+// RANDOM
+const changeBgColor = () => {
+    const index = Math.floor(Math.random() * 6);
+    document.querySelector('body').style.background = colors[index];
+}
